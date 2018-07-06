f236e6e1c7868228d994eebaa36551a716773a0f
Igor Gramovich <igor.gramovich@mrsoft.by>
2018-07-06 11:23:24 +0300

    Добавил в nav-param component выпадающий список параметров

--- src/eos-parameters/parametersSystem/shared/nav-param/nav-param.component.ts
+++ src/eos-parameters/parametersSystem/shared/nav-param/nav-param.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { EOS_PARAMETERS_TAB } from '../consts/eos-parameters.const';
 
 @Component({
     selector: 'eos-nav-param',
     templateUrl: 'nav-param.component.html'
 })
 export class NavParamComponent {
-    infoOpened: boolean;
+    listParams = EOS_PARAMETERS_TAB;
     isWide = false;
     changeState() {
         this.isWide = !this.isWide;
