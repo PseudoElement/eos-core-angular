9566312b1d7260c0ceaaf6397da3f8ebb5dd5b1f
Igor Gramovich <igor.gramovich@mrsoft.by>
2018-07-16 14:15:55 +0300

    Revert "Добавил файлы регистрации плагина в деле"
    This reverts commit a0900f7b86006ee78572a30b3f17e6e38bf8f9b3.

--- src/invokeMenu.js
+++
@@ -1,3 +0,0 @@
-﻿$(function () {
-	$("#dmenu").find(".AspNet-Menu-Item span:contains('Администрирование')").parent().find(".AspNet-Menu-Child").append("<li><a href='../../Classif/index.html'><span class='dot Справочники'>Справочники</span></a></li>")
-});
