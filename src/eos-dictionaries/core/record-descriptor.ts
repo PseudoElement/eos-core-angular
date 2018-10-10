6646bedf2d7fa0ce1054881dd3e8a6d255932529
AlexBojik <tupikinaa@biz-it.ru>
2018-10-10 17:45:58 +0300

    иконки справочников

--- src/eos-dictionaries/core/record-descriptor.ts
+++ src/eos-dictionaries/core/record-descriptor.ts
@@ -178,7 +178,9 @@ export class RecordDescriptor {
                         height: _f.height,
                         foreignKey: _f.foreignKey,
                         forNode: _f.forNode,
-                        default: _f.default
+                        dictionaryId: _f.dictionaryId,
+                        default: _f.default,
+                        password: _f.password
                     };
                 }
             });
