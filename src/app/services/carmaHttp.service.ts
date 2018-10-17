faf838221f2a7c26011406c7175f54750396f61a
Igor Gramovich <igor.gramovich@mrsoft.by>
2018-10-17 15:29:29 +0300

    hot fix

--- src/app/services/carmaHttp.service.ts
+++ src/app/services/carmaHttp.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptionsArgs, Headers } from '@angular/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 export interface Istore {
     Location: string;
