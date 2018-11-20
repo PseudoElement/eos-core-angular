30a24696004a3b98bdaeb110f0b452ac30914e67
hedgehog <derevenskihav@biz-it.ru>
2018-11-20 13:39:13 +0300

    fixed Bug 94421:Каналы передачи. (Обязательные поля)

--- src/eos-dictionaries/card-views/broadcast-channel-card-edit.component.ts
+++ src/eos-dictionaries/card-views/broadcast-channel-card-edit.component.ts
@@ -1,22 +1,103 @@
-import {Component, Injector} from '@angular/core';
-import { BaseCardEditComponent } from './base-card-edit.component';
+///<reference path="../../../node_modules/@angular/core/src/metadata/lifecycle_hooks.d.ts"/>
+import {AfterViewChecked, Component, Injector, OnChanges} from '@angular/core';
+import {BaseCardEditComponent} from './base-card-edit.component';
 
 @Component({
     selector: 'eos-broadcast-channel-card-edit',
     templateUrl: 'broadcast-channel-card-edit.component.html',
 })
-export class BroadcastChannelCardEditComponent extends BaseCardEditComponent {
+export class BroadcastChannelCardEditComponent extends BaseCardEditComponent implements OnChanges, AfterViewChecked {
+
+    private REC_CHANNEL_TYPE = 'rec.CHANNEL_TYPE';
+    private REC_AUTH_METHOD = 'rec.AUTH_METHOD';
+
+    private validatorStore: any[];
+    private requirementsEmail = [
+        'rec.SMTP_EMAIL',
+        'rec.SMTP_SERVER',
+        'rec.SMTP_PORT',
+        'rec.ENCRYPTION_TYPE',
+        'rec.AUTH_METHOD',
+        'rec.POP3_SERVER',
+        'rec.POP3_PORT',
+        'rec.POP3_LOGIN',
+        'rec.POP3_PASSWORD',
+    ];
+    private requirementsFS = [
+        'rec.OUT_FOLDER',
+        'rec.IN_FOLDER',
+    ];
+    private requirementsSMTP = [
+        'rec.SMTP_LOGIN',
+        'rec.SMTP_PASSWORD',
+    ];
 
     constructor(injector: Injector) {
         super(injector);
+        this.validatorStore = [];
+    }
+
+    ngOnChanges() {
+        this.onChannelTypeChanged();
+    }
+
+    ngAfterViewChecked() {
     }
 
     get isEmail(): boolean {
-        return this.getValue('rec.CHANNEL_TYPE') === 'email';
+        const t = this.getValue(this.REC_CHANNEL_TYPE);
+        return t === 'email';
     }
 
     get authMethod(): number {
-        return +this.getValue('rec.AUTH_METHOD');
+        return +this.getValue(this.REC_AUTH_METHOD);
     }
 
+
+    onAuthMethodChanged() {
+        if (this.isEmail && (this.authMethod !== 0)) {
+            this.requirementsSMTP.forEach(k => this.validatorEnable(k));
+        } else {
+            this.requirementsSMTP.forEach(k => this.validatorDisable(k));
+        }
+    }
+
+    onChannelTypeChanged() {
+        if (this.form) {
+            if (this.isEmail) {
+                this.requirementsEmail.forEach(k => this.validatorEnable(k));
+                this.requirementsFS.forEach(k => this.validatorDisable(k));
+            } else {
+                this.requirementsEmail.forEach(k => this.validatorDisable(k));
+                this.requirementsFS.forEach(k => this.validatorEnable(k));
+            }
+            this.onAuthMethodChanged();
+        }
+    }
+
+    private validatorEnable(key: string) {
+        const control = this.inputs[key];
+        if (control) {
+            if (this.form.controls[key].validator) {
+                return;
+            } else {
+                this.form.controls[key].setValidators(this.validatorStore[key]);
+                this.inputs[key].required = true;
+                this.form.controls[key].updateValueAndValidity();
+            }
+        }
+    }
+
+    private validatorDisable(key: string) {
+        const control = this.inputs[key];
+        if (control) {
+            control.required = false;
+            if (this.form.controls[key].validator) {
+                this.validatorStore[key] = this.form.controls[key].validator;
+                this.form.controls[key].clearValidators();
+                this.inputs[key].required = false;
+                this.form.controls[key].updateValueAndValidity();
+            }
+        }
+    }
 }
--- src/eos-dictionaries/consts/dictionaries/broadcast-channel.ts
+++ src/eos-dictionaries/consts/dictionaries/broadcast-channel.ts
@@ -16,96 +16,109 @@ export const BROADCAST_CHANNEL_DICT: IDictionaryDescriptor = Object.assign({}, L
     fields: LINEAR_TEMPLATE.fields.concat([
         Object.assign({}, COMMON_FIELD_NAME, {length: 100}),
         Object.assign({}, COMMON_FIELD_NOTE, {length: 150}),
-    {
-        key: 'CHANNEL_TYPE',
-        type: 'select',
-        title: 'Тип канала',
-        options: CHANNEL_TYPE,
-        required: true,
-        default: 'email',
-        length: 200
-    }, {
-        key: 'PARAMS',
-        type: 'xml',
-        length: 1,
-        title: 'Параметры доставки',
-    }, {
-        key: 'SMTP_EMAIL',
-        type: 'string',
-        title: 'E-mail отправителя',
-        length: 200,
-        pattern: EMAIL,
-     }, {
-        key: 'SMTP_SERVER',
-        type: 'string',
-        title: 'SMTP сервер',
-        length: 255,
-    }, {
-        key: 'SMTP_PORT',
-        type: 'number',
-        title: 'SMTP порт',
-        default: 25
-    }, {
-        key: 'ENCRYPTION_TYPE',
-        type: 'select',
-        options: ENCRYPTION_TYPE,
-        title: 'Использовать следущий тип шифрования:',
-        default: 0
-    }, {
-        key: 'AUTH_METHOD',
-        type: 'select',
-        options: AUTH_METHOD,
-        title: 'Метод аутентификации',
-        default: 0
-    }, {
-        key: 'SMTP_LOGIN',
-        type: 'string',
-        length: 100,
-        title: 'SMTP логин',
-    }, {
-        key: 'SMTP_PASSWORD',
-        type: 'string',
-        length: 100,
-        title: 'SMTP пароль',
-        password: true
-    }, {
-        key: 'SMTP_DELAY',
-        type: 'number',
-        title: 'Задержка, мин',
-    }, {
-        key: 'POP3_SERVER',
-        type: 'string',
-        title: 'POP3 сервер',
-        length: 255,
-    }, {
-        key: 'POP3_PORT',
-        type: 'number',
-        title: 'POP3 порт',
-        default: 110
-    }, {
-        key: 'POP3_ENCRYPTION',
-        type: 'boolean',
-        title: 'Требуется шифрование',
-    }, {
-        key: 'POP3_LOGIN',
-        type: 'string',
-        length: 100,
-        title: 'POP3 логин',
-    }, {
-        key: 'POP3_PASSWORD',
-        type: 'string',
-        password: true,
-        length: 100,
-        title: 'POP3 пароль',
-    }, {
-        key: 'OUT_FOLDER',
-        type: 'string',
-        title: 'Папка исходящих сообщений',
-    }, {
-        key: 'IN_FOLDER',
-        type: 'string',
-        title: 'Папка входящих сообщений',
-    }]),
+        {
+            key: 'CHANNEL_TYPE',
+            type: 'select',
+            title: 'Тип канала',
+            options: CHANNEL_TYPE,
+            required: true,
+            default: 'email',
+            length: 200
+        }, {
+            key: 'PARAMS',
+            type: 'xml',
+            length: 1,
+            title: 'Параметры доставки',
+        }, {
+            key: 'SMTP_EMAIL',
+            type: 'string',
+            title: 'E-mail отправителя',
+            length: 200,
+            required: true,
+            pattern: EMAIL,
+        }, {
+            key: 'SMTP_SERVER',
+            type: 'string',
+            title: 'SMTP сервер',
+            required: true,
+            length: 255,
+        }, {
+            key: 'SMTP_PORT',
+            type: 'number',
+            required: true,
+            title: 'SMTP порт',
+            default: 25
+        }, {
+            key: 'ENCRYPTION_TYPE',
+            type: 'select',
+            options: ENCRYPTION_TYPE,
+            required: true,
+            title: 'Использовать следущий тип шифрования:',
+            default: 0
+        }, {
+            key: 'AUTH_METHOD',
+            type: 'select',
+            options: AUTH_METHOD,
+            required: true,
+            title: 'Метод аутентификации',
+            default: 0
+        }, {
+            key: 'SMTP_LOGIN',
+            type: 'string',
+            length: 100,
+            title: 'SMTP логин',
+            required: true,
+        }, {
+            key: 'SMTP_PASSWORD',
+            type: 'string',
+            length: 100,
+            title: 'SMTP пароль',
+            password: true,
+            required: true,
+        }, {
+            key: 'SMTP_DELAY',
+            type: 'number',
+            title: 'Задержка, мин',
+        }, {
+            key: 'POP3_SERVER',
+            type: 'string',
+            title: 'POP3 сервер',
+            length: 255,
+            required: true,
+        }, {
+            key: 'POP3_PORT',
+            type: 'number',
+            title: 'POP3 порт',
+            required: true,
+            default: 110
+        }, {
+            key: 'POP3_ENCRYPTION',
+            type: 'boolean',
+            title: 'Требуется шифрование',
+        }, {
+            key: 'POP3_LOGIN',
+            type: 'string',
+            length: 100,
+            title: 'POP3 логин',
+            required: true,
+        }, {
+            key: 'POP3_PASSWORD',
+            type: 'string',
+            password: true,
+            length: 100,
+            title: 'POP3 пароль',
+            required: true,
+        }, {
+            key: 'OUT_FOLDER',
+            type: 'string',
+            title: 'Папка исходящих сообщений',
+            required: true,
+        }, {
+            key: 'IN_FOLDER',
+            type: 'string',
+            title: 'Папка входящих сообщений',
+            required: true,
+        }]),
     editFields: ['CLASSIF_NAME', 'NOTE', 'CHANNEL_TYPE',
         'SMTP_EMAIL', 'SMTP_SERVER', 'SMTP_PORT', 'ENCRYPTION_TYPE', 'AUTH_METHOD', 'SMTP_LOGIN', 'SMTP_PASSWORD', 'SMTP_DELAY',
         'POP3_SERVER', 'POP3_PORT', 'POP3_ENCRYPTION', 'POP3_LOGIN', 'POP3_PASSWORD',
