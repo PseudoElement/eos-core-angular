a7c2133a12f6ca9a44d19e7565629ea4f820dee8
hedgehog <derevenskihav@biz-it.ru>
2018-11-26 15:17:40 +0300

    fix Bug 94030:Организационно-правовые формы.

--- src/eos-dictionaries/consts/dictionaries/nadzor/org-prav-form.consts.ts
+++ src/eos-dictionaries/consts/dictionaries/nadzor/org-prav-form.consts.ts
@@ -1,12 +1,16 @@
 import { IDictionaryDescriptor } from 'eos-dictionaries/interfaces/index';
 import {NADZOR_TEMPLATE} from '../nadzor-template';
-import {COMMON_FIELD_CODE} from '../_common';
+import {COMMON_FIELD_CODE } from '../_common';
 
 export const NP_OPF_CL: IDictionaryDescriptor = Object.assign({}, NADZOR_TEMPLATE, {
     id: 'org-prav-form',
     apiInstance: 'NP_OPF_CL',
     title: 'Организационно-правовые формы',
     visible: true,
-    fields: [...NADZOR_TEMPLATE.fields, COMMON_FIELD_CODE],
+    fields: [...NADZOR_TEMPLATE.fields,
+        Object.assign({}, COMMON_FIELD_CODE, {
+            required: true,
+        }),
+        ],
     editFields: ['CODE', 'CLASSIF_NAME', 'NOTE']
  });
