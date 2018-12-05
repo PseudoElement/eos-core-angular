c547f832992123771b48b675ece1f5b4795e4a26
AlexBojik <tupikinaa@biz-it.ru>
2018-12-05 18:16:40 +0300

    Bug 94416

--- src/eos-dictionaries/node-info/node-info.component.ts
+++ src/eos-dictionaries/node-info/node-info.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
-import { BaseNodeInfoComponent } from './base-node-info';
+import {Component} from '@angular/core';
+import {BaseNodeInfoComponent} from './base-node-info';
 
 @Component({
     selector: 'eos-node-info',
     templateUrl: 'node-info.component.html',
 })
 
-export class NodeInfoComponent extends BaseNodeInfoComponent { }
+export class NodeInfoComponent extends BaseNodeInfoComponent {
+    value(key): string {
+        let res = this.nodeDataFull.rec[key];
+
+        const field = this.fieldsDescriptionFull.rec[key];
+        if (field.type === this.fieldTypes.select && field.options.length) {
+            res = field.options.find((op) => op.value === res).title;
+        }
+
+        return res;
+    }
+}
