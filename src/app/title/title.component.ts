bd0256bb5282ef61520043fdd4b8c7b58b67200b
Igor Gramovich <igor.gramovich@mrsoft.by>
2018-12-05 10:43:01 +0300

    Fix 95480 Bug Возврат на главную дело веб

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,26 +1,17 @@
 import { Component } from '@angular/core';
 
-import { EosDeskService } from '../services/eos-desk.service';
+// import { EosDeskService } from '../services/eos-desk.service';
 
 @Component({
     selector: 'eos-title',
     templateUrl: 'title.component.html'
 })
 export class TitleComponent {
-    currentDesk: string;
     /* todo: define it or remove. Mocked now*/
     title = 'Администрирование системы';
-
-    constructor(private _deskSrv: EosDeskService) {
-        this._deskSrv.selectedDesk.subscribe((link) => {
-            if (link) {
-                this.currentDesk = '/desk/';
-                if (link) {
-                    this.currentDesk += link.id;
-                }
-            } else {
-                this.currentDesk = '#';
-            }
-        });
+    openDelo() {
+        let url = document.location.href.split('#')[0];
+        url = url.slice(0, url.lastIndexOf('Classif')) + 'main.aspx';
+        document.location.assign(url);
     }
 }
