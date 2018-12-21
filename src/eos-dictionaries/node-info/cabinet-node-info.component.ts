fc897e60da85f59f473d845b6655d95525846ed4
AlexBojik <tupikinaa@biz-it.ru>
2018-12-21 12:11:37 +0300

    Bug 93713:Кабинеты. В столбце подробной информации не выводятся владельцы и пользователи кабинета

--- src/eos-dictionaries/node-info/cabinet-node-info.component.ts
+++ src/eos-dictionaries/node-info/cabinet-node-info.component.ts
@@ -8,4 +8,28 @@ import { BaseNodeInfoComponent } from './base-node-info';
 })
 export class CabinetNodeInfoComponent extends BaseNodeInfoComponent {
     update: boolean;
+
+    get cabinetName(): string {
+        return this.nodeDataShort.rec['FULLNAME'] || this.nodeDataShort.rec['CABINET_NAME'];
+    }
+
+    get departmentName(): string {
+        return this.nodeDataFull.department['FULLNAME'] || this.nodeDataFull.department['CLASSIF_NAME'];
+    }
+
+    routerLinkDepartment(due): string {
+        return this._routerLink('departments', due);
+    }
+
+    routerLinkUser(isn_user): string {
+        return this._routerLink('users', isn_user);
+    }
+
+    isOwner(owner): boolean {
+        return this.nodeDataFull.rec['ISN_CABINET'] === owner['ISN_CABINET'];
+    }
+
+    private _routerLink(dict, key): string {
+        return '/spravochniki/' + dict + '/' + key + '/view';
+    }
 }
