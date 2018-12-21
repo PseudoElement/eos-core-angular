8c35156155d8d2f49079c97db1383864f080cae1
Andrei-Vidnik <andrei.vidnik@mrsoft.by>
2018-12-21 15:43:55 +0300

    fix bug  максимальное количество записей

--- src/eos-common/dynamic-form-input/dynamic-input-increment-number.component.ts
+++ src/eos-common/dynamic-form-input/dynamic-input-increment-number.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 import { DynamicInputBase } from './dynamic-input-base';
+import { Validators } from '@angular/forms';
 
 @Component({
     selector: 'eos-dynamic-input-increment-number',
     templateUrl: 'dynamic-input-increment-number.component.html'
 })
-export class DynamicInputNumberIncrementComponent extends DynamicInputBase {
+export class DynamicInputNumberIncrementComponent extends DynamicInputBase  implements OnChanges {
+    constructor() {
+        super();
+    }
     onIncrease() {
         if (this.control.enabled) {
-            this.control.patchValue(String(+this.control.value + 1));
+            if (!isNaN(this.control.value)) {
+                this.control.patchValue(String(+this.control.value + 1));
+            } else {
+                this.control.patchValue('0');
+            }
             this.control.markAsDirty();
         }
     }
     onDecrease() {
         if (this.control.enabled) {
-            this.control.patchValue(String(+this.control.value - 1));
+            if (!isNaN(this.control.value)) {
+                this.control.patchValue(String(+this.control.value - 1));
+            } else {
+                this.control.patchValue('0');
+            }
             this.control.markAsDirty();
         }
     }
+    ngOnChanges() {
+           if ( !this.input.pattern) {
+                 this.control.setValidators(Validators.pattern(/^\d{0,3}$|^9900$/));
+           }
+    }
+
 }
