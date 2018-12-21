070a4f78b604bbcba8a991bf2e076926ce447161
Igor Gramovich <igor.gramovich@mrsoft.by>
2018-12-21 09:27:59 +0300

    Реализация подразделений в абсолютных правах

--- src/eos-user-params/rights-delo/rights-delo-absolute-rights/rights-delo-absolute-rights.component.ts
+++ src/eos-user-params/rights-delo/rights-delo-absolute-rights/rights-delo-absolute-rights.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserParamApiSrv } from 'eos-user-params/shared/services/user-params-api.service';
 import { ABSOLUTE_RIGHTS, CONTROL_ALL_NOTALL } from '../shared-rights-delo/consts/absolute-rights.consts';
-// import { EosUtils } from 'eos-common/core/utils';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { AbsoluteRightsDirectoryComponent } from './absolute-rights-directory-modal/absolute-rights-directory-modal.component';
 import { InputParamControlService } from 'eos-user-params/shared/services/input-param-control.service';
@@ -11,6 +10,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { E_RIGHT_DELO_ACCESS_CONTENT } from '../shared-rights-delo/interfaces/right-delo.intefaces';
 import { RadioInput } from 'eos-common/core/inputs/radio-input';
 import { Subscription } from 'rxjs/Subscription';
+import { NodeAbsoluteRight } from './node-absolute';
+import { EosMessageService } from 'eos-common/services/eos-message.service';
+import { SUCCESS_SAVE_MESSAGE_SUCCESS } from 'eos-common/consts/common.consts';
 
 @Component({
     selector: 'eos-rights-delo-absolute-rights',
@@ -23,8 +25,8 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
     directoryModal: BsModalRef;
     arrDeloRight: string[];
     arrNEWDeloRight: string[];
-    selectedNode: IInputParamControl;
-    fields: IInputParamControl[] = ABSOLUTE_RIGHTS;
+    selectedNode: NodeAbsoluteRight; // текущий выбранный элемент
+    fields: IInputParamControl[];
     inputs;
     inputAll;
     form: FormGroup;
@@ -33,9 +35,11 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
     subForm: Subscription;
     queryForSave = [];
     rightContent: boolean;
+    listRight: NodeAbsoluteRight[] = [];
 
 
     constructor (
+        private _msgSrv: EosMessageService,
         private _userParamsSetSrv: UserParamsService,
         private _modalSrv: BsModalService,
         private apiSrv: UserParamApiSrv,
@@ -44,46 +48,62 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
             this.curentUser = this._userParamsSetSrv.curentUser;
             this.arrDeloRight = this.curentUser['DELO_RIGHTS'].split('');
             this.arrNEWDeloRight = this.curentUser['DELO_RIGHTS'].split('');
-            this._writeValue(this.fields);
+            this.fields = this._writeValue(ABSOLUTE_RIGHTS);
             this.inputs = this._inputCtrlSrv.generateInputs(this.fields);
             this.form = this._inputCtrlSrv.toFormGroup(this.inputs);
+            this.listRight = this._createList(ABSOLUTE_RIGHTS);
             this.subForm = this.form.valueChanges
              .subscribe(data => {
-                 // tslint:disable-next-line:forin
                  for (const key in  data) {
                      if (+this.arrNEWDeloRight[key] > 1 && data[key]) {
                          continue;
                      }
                     this.arrNEWDeloRight[key] = (+data[key]).toString();
                  }
-                 this._checkChenge();
+                 this.checkChange();
+                 setTimeout(() => {
+                     this._viewContent();
+                 }, 0);
              });
         }
     ngOnInit() {
-        this.selectNode(this.fields[0]);
+        this.selectNode(this.listRight[0]);
         this.inputAll = {all: new RadioInput(CONTROL_ALL_NOTALL)};
     }
     ngOnDestroy() {
         this.subForm.unsubscribe();
     }
     submit() {
-        this.queryForSave.push({
-            method: 'MERGE',
-            requestUri: `USER_CL(${this._userParamsSetSrv.userContextId})`,
-            data: {
-                DELO_RIGHTS: this.arrNEWDeloRight.join('')
+        if (this.arrNEWDeloRight.join('') !== this.arrDeloRight.join('')) {
+            this.queryForSave.push({
+                method: 'MERGE',
+                requestUri: `USER_CL(${this._userParamsSetSrv.userContextId})`,
+                data: {
+                    DELO_RIGHTS: this.arrNEWDeloRight.join('')
+                }
+            });
+        }
+        this.listRight.forEach((node: NodeAbsoluteRight) => {
+            if (node.touched) {
+                node.change.forEach(ch => {
+                    this.queryForSave.push(this._createBatch(ch));
+                });
+                node.deleteChange();
             }
         });
+
         this.apiSrv.setData(this.queryForSave)
         .then(data => {
             this.queryForSave = [];
             this.btnDisabled = true;
+            this._msgSrv.addNewMessage(SUCCESS_SAVE_MESSAGE_SUCCESS);
         })
         .catch(e => {
             console.log(e);
         });
     }
     cancel() {
+        // console.log(this.listRight);
         // console.log('cancel()');
         // console.log('new', this.arrNEWDeloRight.join(''));
         // console.log('old', this.arrDeloRight.join(''));
@@ -93,31 +113,34 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
         //     }
         // });
     }
-    clickLable(event, item: IInputParamControl, control: FormControl) {
+    clickLable(event, item: NodeAbsoluteRight) {
         event.preventDefault();
         event.stopPropagation();
-        if (event.target.tagName === 'LABEL') {
+        if (event.target.tagName === 'LABEL') { // click to label
             this.selectNode(item);
         }
-        if (event.target.tagName === 'SPAN') {
-            const value = !control.value;
+        if (event.target.tagName === 'SPAN') { // click to checkbox
+            const value = !item.value;
+            if (!value && (item.contentProp === E_RIGHT_DELO_ACCESS_CONTENT.department || E_RIGHT_DELO_ACCESS_CONTENT.departmentCardAuthor || E_RIGHT_DELO_ACCESS_CONTENT.departmentCardAuthorSentProject)) {
+                this._deleteAllDep(item);
+            }
+
             item.value = value;
-            control.patchValue(value);
-            if (item === this.selectedNode) {
-                this._viewContent();
+
+            if (item !== this.selectedNode && item.isCreate) {
+                this.selectNode(item);
             }
         }
     }
-    selectNode(node: IInputParamControl) {
+    selectNode(node: NodeAbsoluteRight) {
         if (this.selectedNode !== node) {
             if (this.selectedNode) {
-                this.selectedNode.data['isSelected'] = false;
+                this.selectedNode.isSelected = false;
             }
             this.selectedNode = node;
-            this.selectedNode.data['isSelected'] = true;
+            this.selectedNode.isSelected = true;
             this._viewContent();
         }
-        // console.log(this.selectedNode);
     }
     openClessifModal() {
         this.directoryModal = this._modalSrv.show(AbsoluteRightsDirectoryComponent, {
@@ -128,32 +151,28 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
             this.directoryModal.hide();
         });
     }
-    createBatch(data) {
-        // console.log(data);
-        const newDep = data['newDep'];
-        const d = Object.assign({}, data['node']);
-        delete d.department;
-        delete d.isSelected;
-        const str = newDep ? '' : `('${this._userParamsSetSrv.userContextId} ${d['DUE']} ${d['FUNC_NUM']}')`;
-        const batch = {
-            method: newDep ? 'POST' : 'DELETE',
-            requestUri: `USER_CL(${this._userParamsSetSrv.userContextId})/USERDEP_List${str}`,
-        };
-        if (newDep) {
-            batch['data'] = d;
-        }
-        // console.log(batch);
-        this.queryForSave.push(batch);
-        this._checkChenge();
-    }
-    private _writeValue(fields: IInputParamControl[]) {
-        fields.forEach((node: IInputParamControl) => {
-            node['value'] = !!+this.arrDeloRight[+node['key']];
+    checkChange() {
+        let c = false;
+        this.listRight.forEach(li => { // проверяем список на изменения
+            if (li.touched) {
+                c = true;
+            }
         });
+        if (this.arrNEWDeloRight.join('') !== this.arrDeloRight.join('')) {
+            c = true;
+        }
+        this.btnDisabled = !c;
+    }
+    private _writeValue(constanta: IInputParamControl[]): IInputParamControl[] {
+        const fields = [];
+        constanta.forEach((node: IInputParamControl) => {
+            fields.push(Object.assign({value: !!+this.arrDeloRight[+node['key']]}, node));
+        });
+        return fields;
     }
     private _viewContent() {
         this.rightContent = false;
-        switch (this.selectedNode.data['rightContent']) {
+        switch (this.selectedNode.contentProp) {
             case E_RIGHT_DELO_ACCESS_CONTENT.all:
                 if (this.formGroupAll) {
                     this.subs['all'].unsubscribe();
@@ -168,24 +187,58 @@ export class RightsDeloAbsoluteRightsComponent implements OnInit, OnDestroy {
                 this.subs['all'] = this.formGroupAll.valueChanges
                     .subscribe(data => {
                         this.arrNEWDeloRight[+this.selectedNode.key] = data['all'];
-                        this._checkChenge();
+                        this.checkChange();
                     });
                 this.rightContent = true;
                 break;
             case E_RIGHT_DELO_ACCESS_CONTENT.department:
             case E_RIGHT_DELO_ACCESS_CONTENT.departmentCardAuthorSentProject:
+                if (this.selectedNode.value) {
                     setTimeout(() => {
                         this.rightContent = true;
                     }, 0);
+                }
                 break;
         }
     }
-    private _checkChenge() {
-        if (this.arrNEWDeloRight.join('') !== this.arrDeloRight.join('') || this.queryForSave.length) {
-            // console.log('chenged');
-            this.btnDisabled = false;
-        } else {
-            this.btnDisabled = true;
+    private _createList(constanta: IInputParamControl[]): NodeAbsoluteRight[] {
+        const fields = [];
+        constanta.forEach((node: IInputParamControl) => {
+            fields.push(new NodeAbsoluteRight(node, !!+this.arrDeloRight[+node['key']], this.form.get(node['key'])));
+        });
+        return fields;
+    }
+    private _deleteAllDep(item: NodeAbsoluteRight) {
+        const list = [];
+        this.curentUser.USERDEP_List = this.curentUser.USERDEP_List.filter(li => {
+            if (li['FUNC_NUM'] === +item.key + 1) {
+                list.push(li);
+            } else {
+                return true;
+            }
+        });
+
+        list.forEach(li => {
+            item.pushChange({
+                userDep: li,
+                createEntity: false
+            });
+        });
+        this.checkChange();
+    }
+
+    private _createBatch(data) {
+        const create = data['createEntity'];
+        const userDep = Object.assign({}, data['userDep']);
+
+        const str = create ? '' : `('${this._userParamsSetSrv.userContextId} ${userDep['DUE']} ${userDep['FUNC_NUM']}')`;
+        const batch = {
+            method: create ? 'POST' : 'DELETE',
+            requestUri: `USER_CL(${this._userParamsSetSrv.userContextId})/USERDEP_List${str}`,
+        };
+        if (create) {
+            batch['data'] = userDep;
         }
+        return batch;
     }
 }
