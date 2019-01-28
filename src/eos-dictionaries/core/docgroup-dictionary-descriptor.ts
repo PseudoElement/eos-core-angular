dc0805f385afec03d238a3d98d0363c2ffdc41c0
tupikinaa <tupikinaa@biz-it.ru>
2019-01-28 14:47:56 +0300

    Bug 93699:Группы документов. При создании дочерней записи с вершины не берется индекс

--- src/eos-dictionaries/core/docgroup-dictionary-descriptor.ts
+++ src/eos-dictionaries/core/docgroup-dictionary-descriptor.ts
@@ -4,15 +4,15 @@ import { EosUtils } from 'eos-common/core/utils';
 
 const RC_TYPE = 'RC_TYPE';
 const RC_TYPE_NODE = 'RC_TYPE_NODE';
+const inheritFiields = [RC_TYPE, 'DOCGROUP_INDEX', 'ACCESS_MODE', 'ACCESS_MODE_FIXED'];
 
 export class DocgroupDictionaryDescriptor extends TreeDictionaryDescriptor {
     getNewRecord(preSetData: {}, parentNode: EosDictionaryNode): {} {
         const newPreset = {};
         EosUtils.deepUpdate(newPreset, preSetData);
-        if (parentNode && parentNode.data.rec[RC_TYPE]) {
-            Object.assign(newPreset['rec'],
-                {[RC_TYPE]: parentNode.data.rec[RC_TYPE],
-                [RC_TYPE_NODE]: parentNode.data.rec[RC_TYPE]});
+        if (parentNode) {
+            inheritFiields.forEach((f) => this._fillParentField(newPreset, parentNode, f));
+            Object.assign(newPreset['rec'], {[RC_TYPE_NODE]: newPreset['rec'][RC_TYPE]});
         }
         return super.getNewRecord(newPreset, parentNode);
     }
@@ -20,10 +20,14 @@ export class DocgroupDictionaryDescriptor extends TreeDictionaryDescriptor {
     getData(query?: any, order?: string, limit?: number): Promise<any[]> {
         return super.getData(query, order, limit)
             .then((data) => {
-                data.forEach(d => {
-                    d['RC_TYPE_NODE'] = d['RC_TYPE'];
-                });
+                data.forEach((d) => d[RC_TYPE_NODE] = d[RC_TYPE]);
                 return data;
             });
     }
+
+    private _fillParentField(preset, parentNode, fieldName) {
+        if (parentNode.data.rec[fieldName]) {
+            Object.assign(preset['rec'], {[fieldName]: parentNode.data.rec[fieldName]});
+        }
+     }
 }
