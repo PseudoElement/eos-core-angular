51f3a9e1afdb76a23585c9ab26e53385ccd5145a
Igor Gramovich <igor.gramovich@mrsoft.by>
2019-02-12 15:52:19 +0300

    Bug 97863 Защита роутов

---
+++ src/app/consts/permission.consts.ts
@@ -0,0 +1,21 @@
+import { E_TECH_RIGHT } from 'eos-rest/interfaces/rightName';
+
+export interface IKeyRightTech {
+    key: E_TECH_RIGHT;
+    name: string;
+}
+
+export const KEY_RIGHT_TECH = {
+    'parameters': {
+        key: E_TECH_RIGHT.SystemParms,
+        name: 'Настройки системы'
+    },
+    'user_param': {
+        key: E_TECH_RIGHT.Users,
+        name: 'Пользователи'
+    },
+    'user-params-set': {
+        key: E_TECH_RIGHT.Users,
+        name: 'Пользователи'
+    },
+};
