bdabeef8086a1cc1a7ce4a7be720422f1fe8620e
Igor Gramovich <igor.gramovich@mrsoft.by>
2019-03-13 19:20:50 +0300

    отобразил список funcNum

--- src/eos-user-params/rights-delo/rights-delo-cards/rights-delo-cards.component.ts
+++ src/eos-user-params/rights-delo/rights-delo-cards/rights-delo-cards.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserParamsService } from 'eos-user-params/shared/services/user-params.service';
+import { IParamUserCl } from 'eos-user-params/shared/intrfaces/user-parm.intterfaces';
+import { Router } from '@angular/router';
+import { CARD_FUNC_LIST } from './card-func-list.consts';
+import { FuncNum } from './funcnum.model';
 // import { Subject } from 'rxjs/Subject';
 
 @Component({
@@ -7,16 +12,67 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 
 export class RightsDeloCardsComponent implements OnInit, OnDestroy {
+    public selfLink: string;
+    public titleHeader: string;
+    public isLoading: boolean;
+    public btnDisabled: boolean = true;
+    public pageState: 'LOADING' | 'EMPTY' | 'VIEW' = 'LOADING';
+    public editableUser: IParamUserCl;
+    public funcList: FuncNum[];
+    public editMode: boolean = false;
+    private _selectedFuncNum: FuncNum;
     // private _ngUnsubscribe: Subject<any> = new Subject();
     constructor(
-        // private _userParamsSetSrv: UserParamsService,
+        private _userParamsSetSrv: UserParamsService,
         // private servApi: UserParamApiSrv,
         // private _inputCtrlSrv: InputParamControlService,
-    ) {}
-    ngOnInit() {
-        console.log('ngOnInit()');
+        private _router: Router,
+    ) {
+        this.selfLink = this._router.url.split('?')[0];
+    }
+    async ngOnInit() {
+        // получение пользователя
+        await Promise.resolve(); // this._userParamsSetSrv.getUserIsn();
+        this.editableUser = this._userParamsSetSrv.curentUser;
+        this.titleHeader = `${this.editableUser.SURNAME_PATRON} - Права в картотеках`;
+        if (!this.editableUser.USERCARD_List || !this.editableUser.USERCARD_List.length) {
+            this.pageState = 'EMPTY';
+            return;
+        }
+        this.funcList = CARD_FUNC_LIST.map(node => new FuncNum(node));
+        this.pageState = 'VIEW';
+
+        console.log('ngOnInit() end');
     }
     ngOnDestroy() {
         console.log('ngOnDestroy()');
+    }
+    submit() {
+        console.log('submit()');
+    }
+    cancel() {
+        this.editMode = false;
+        this._selectedFuncNum.isSelected = false;
+        this._selectedFuncNum = null;
+    }
+    edit() {
+        this.editMode = true;
+        this.selectFuncNum(this.funcList[0]);
+    }
+    close() {
+        this._router.navigate(['user_param']);
+    }
+    selectFuncNum(node: FuncNum) {
+        if (!this.editMode) {
+            return;
+        }
+        if (this._selectedFuncNum === node) {
+            return;
+        }
+        if (this._selectedFuncNum) {
+            this._selectedFuncNum.isSelected = false;
+        }
+        this._selectedFuncNum = node;
+        this._selectedFuncNum.isSelected = true;
     }
 }
