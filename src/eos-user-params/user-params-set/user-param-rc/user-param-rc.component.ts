1e925313bed5ceb8823e4bdf01b5ff28f6e6435d
Andrei-Vidnik <andrei.vidnik@mrsoft.by>
2019-03-25 13:51:06 +0300

    РК - изменения в header

--- src/eos-user-params/user-params-set/user-param-rc/user-param-rc.component.ts
+++ src/eos-user-params/user-params-set/user-param-rc/user-param-rc.component.ts
@@ -8,13 +8,26 @@ import { UserParamRCSrv } from '../shared-user-param/services/user-param-rc.serv
 
 export class UserParamRCComponent extends UserParamRCSrv implements OnDestroy {
     prepInputsAttach;
+    selfLink;
+    link;
     constructor( injector: Injector ) {
         super(injector);
+        this.selfLink = this._router.url.split('?')[0];
+        this.link = this._userParamsSetSrv.userContextId;
     }
 
     ngOnDestroy() {
         this._ngUnsubscribe.next();
         this._ngUnsubscribe.complete();
+    }
+    cancel($event?) {
+        this.flagEdit = false;
+        super.cancel();
+    }
+    edit($event) {
+        this.flagEdit = $event;
+        this.editMode();
+        this.checRcShowRes();
     }
 
 }
