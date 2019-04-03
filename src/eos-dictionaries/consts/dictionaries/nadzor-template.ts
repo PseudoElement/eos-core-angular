a469be86fb5cae3d55b8c7e37b4d17d2e1fce395
hedgehog <derevenskihav@biz-it.ru>
2019-04-03 12:15:46 +0300

    fix Bug 100853*:Справочники Надзора.

--- src/eos-dictionaries/consts/dictionaries/nadzor-template.ts
+++ src/eos-dictionaries/consts/dictionaries/nadzor-template.ts
@@ -33,7 +33,7 @@ export const NADZOR_TEMPLATE: IDictionaryDescriptor = {
         title: 'ID',
     },
     Object.assign({}, COMMON_FIELD_NAME, {
-        length: 250,
+        length: 64,
     }),
     Object.assign({}, COMMON_FIELD_NOTE, {
             length: 500,
