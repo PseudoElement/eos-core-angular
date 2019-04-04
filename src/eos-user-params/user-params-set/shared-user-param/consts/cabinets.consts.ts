3a853a33b9c2c6f9b9f21ae36de39bec838d0a0b
Andrei-Vidnik <andrei.vidnik@mrsoft.by>
2019-04-04 18:11:44 +0300

    настройки - кабинеты - рефактор компонента

--- src/eos-user-params/user-params-set/shared-user-param/consts/cabinets.consts.ts
+++ src/eos-user-params/user-params-set/shared-user-param/consts/cabinets.consts.ts
@@ -7,8 +7,6 @@ export const CABINETS_USER: IBaseUsers = {
     title: 'Кабинеты',
     apiInstance: 'USER_PARMS',
     disabledFields: [
-        'HILITE_RESOLUTION_INCREMENT',
-        'HILITE_PRJ_RC_INCREMENT'
     ],
     fields: [
         {
@@ -65,40 +63,41 @@ export const CABINETS_USER: IBaseUsers = {
             key: 'HILITE_RESOLUTION',
             type: 'numberIncrement',
             title: '',
-            readonly: true,
         },
         {
             key: 'HILITE_RESOLUTION_BOOLEAN',
             type: 'boolean',
             title: 'Подсвечивать записи с поручениями РК, срок исполнения которых истекает через'
         },
+        // {
+        //     key: 'HILITE_RESOLUTION_INCREMENT',
+        //     type: 'numberIncrement',
+        //     title: '',
+        //     pattern: REG_MAX_SIZE1
+        // },
         {
-            key: 'HILITE_RESOLUTION_INCREMENT',
+            key: 'HILITE_PRJ_RC',
             type: 'numberIncrement',
             title: '',
             pattern: REG_MAX_SIZE1
-        },
-        {
-            key: 'HILITE_PRJ_RC',
-            type: 'boolean',
-            title: ''
         },
         {
             key: 'HILITE_PRJ_RC_BOOLEAN',
             type: 'boolean',
             title: 'Подсвечивать записи в папке "Управление проектами", срок которых истекает через'
         },
-        {
-            key: 'HILITE_PRJ_RC_INCREMENT',
-            type: 'numberIncrement',
-            title: '',
-            pattern: REG_MAX_SIZE1
-        },
+        // {
+        //     key: 'HILITE_PRJ_RC_INCREMENT',
+        //     type: 'numberIncrement',
+        //     title: '',
+        //     pattern: REG_MAX_SIZE1
+        // },
         {
             key: 'CABSORT_ISN_DOCGROUP_LIST',
             type: 'select',
             title: '',
             options: [
+                {value: '', title: ''}
             ]
         },
         {
@@ -253,73 +252,73 @@ export const CABINETS_USER: IBaseUsers = {
             ]
         },
     ],
-    fieldsChild: [
-        {
-            key: 'FOLDERCOLORSTATUS_RECEIVED',
-            type: 'boolean',
-            title: 'Поступившие'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_FOR_EXECUTION',
-            type: 'boolean',
-            title: 'На исполнение'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_UNDER_CONTROL',
-            type: 'boolean',
-            title: 'На контроле'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_HAVE_LEADERSHIP',
-            type: 'boolean',
-            title: 'У руководства'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_FOR_CONSIDERATION',
-            type: 'boolean',
-            title: 'На рассмотрение'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_INTO_THE_BUSINESS',
-            type: 'boolean',
-            title: 'В дело'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_PROJECT_MANAGEMENT',
-            type: 'boolean',
-            title: 'Управление проектами'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_ON_SIGHT',
-            type: 'boolean',
-            title: 'На визировании'
-        },
-        {
-            key: 'FOLDERCOLORSTATUS_ON_THE_SIGNATURE',
-            type: 'boolean',
-            title: 'На подписи'
-        },
-        {
-            key: 'HILITE_RESOLUTION_BOOLEAN',
-            type: 'boolean',
-            title: 'Подсвечивать записи с поручениями РК, срок исполнения которых истекает через'
-        },
-        {
-            key: 'HILITE_RESOLUTION_INCREMENT',
-            type: 'numberIncrement',
-            title: '',
-            pattern: REG_MAX_SIZE1
-        },
-        {
-            key: 'HILITE_PRJ_RC_BOOLEAN',
-            type: 'boolean',
-            title: 'Подсвечивать записи в папке "Управление проектами", срок которых истекает через'
-        },
-        {
-            key: 'HILITE_PRJ_RC_INCREMENT',
-            type: 'numberIncrement',
-            title: '',
-            pattern: REG_MAX_SIZE1,
-        },
-    ]
+    // fieldsChild: [
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_RECEIVED',
+    //         type: 'boolean',
+    //         title: 'Поступившие'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_FOR_EXECUTION',
+    //         type: 'boolean',
+    //         title: 'На исполнение'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_UNDER_CONTROL',
+    //         type: 'boolean',
+    //         title: 'На контроле'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_HAVE_LEADERSHIP',
+    //         type: 'boolean',
+    //         title: 'У руководства'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_FOR_CONSIDERATION',
+    //         type: 'boolean',
+    //         title: 'На рассмотрение'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_INTO_THE_BUSINESS',
+    //         type: 'boolean',
+    //         title: 'В дело'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_PROJECT_MANAGEMENT',
+    //         type: 'boolean',
+    //         title: 'Управление проектами'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_ON_SIGHT',
+    //         type: 'boolean',
+    //         title: 'На визировании'
+    //     },
+    //     {
+    //         key: 'FOLDERCOLORSTATUS_ON_THE_SIGNATURE',
+    //         type: 'boolean',
+    //         title: 'На подписи'
+    //     },
+    //     {
+    //         key: 'HILITE_RESOLUTION_BOOLEAN',
+    //         type: 'boolean',
+    //         title: 'Подсвечивать записи с поручениями РК, срок исполнения которых истекает через'
+    //     },
+    //     {
+    //         key: 'HILITE_RESOLUTION_INCREMENT',
+    //         type: 'numberIncrement',
+    //         title: '',
+    //         pattern: REG_MAX_SIZE1
+    //     },
+    //     {
+    //         key: 'HILITE_PRJ_RC_BOOLEAN',
+    //         type: 'boolean',
+    //         title: 'Подсвечивать записи в папке "Управление проектами", срок которых истекает через'
+    //     },
+    //     {
+    //         key: 'HILITE_PRJ_RC_INCREMENT',
+    //         type: 'numberIncrement',
+    //         title: '',
+    //         pattern: REG_MAX_SIZE1,
+    //     },
+    // ]
 };
--- src/eos-user-params/user-params-set/user-param-cabinets/user-param-cabinets.component.ts
+++ src/eos-user-params/user-params-set/user-param-cabinets/user-param-cabinets.component.ts
@@ -1,37 +1,146 @@
-import { Component, Injector, OnDestroy } from '@angular/core';
-import { UserParamCabinetsSrv } from '../shared-user-param/services/user-param-cabinets.service';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormHelperService } from '../../shared/services/form-helper.services';
+import { Subject } from 'rxjs/Subject';
+import { UserParamsService } from '../../shared/services/user-params.service';
+import { CABINETS_USER } from '../shared-user-param/consts/cabinets.consts';
+import { EosDataConvertService } from 'eos-dictionaries/services/eos-data-convert.service';
+import { FormGroup } from '@angular/forms';
+import { InputControlService } from 'eos-common/services/input-control.service';
 @Component({
     selector: 'eos-user-param-cabinets',
-    templateUrl: 'user-param-cabinets.component.html'
+    templateUrl: 'user-param-cabinets.component.html',
+    providers: [FormHelperService]
 })
 
-export class UserParamCabinetsComponent extends UserParamCabinetsSrv implements OnDestroy {
+export class UserParamCabinetsComponent implements OnDestroy, OnInit {
     userId: string;
     isChanged: boolean;
     prepInputsAttach;
     selfLink;
     link;
+    public prapareData;
+    public prepareInputs;
+    public inputs;
+    public form: FormGroup;
+    readonly fieldGroupsForCabinets: string[] = ['Папки', 'Поручения'];
+    public currTab: number = 0;
+    public allData;
+    public mapChanges = new Map();
+    readonly fieldsKeys: Map<string, number> = new Map([['FOLDERCOLORSTATUS_RECEIVED', 0],
+    ['FOLDERCOLORSTATUS_FOR_EXECUTION', 1], ['FOLDERCOLORSTATUS_UNDER_CONTROL', 2], ['FOLDERCOLORSTATUS_HAVE_LEADERSHIP', 3],
+    ['FOLDERCOLORSTATUS_FOR_CONSIDERATION', 4], ['FOLDERCOLORSTATUS_INTO_THE_BUSINESS', 5], ['FOLDERCOLORSTATUS_PROJECT_MANAGEMENT', 6],
+    ['FOLDERCOLORSTATUS_ON_SIGHT', 7], ['FOLDERCOLORSTATUS_ON_THE_SIGNATURE', 8]]);
+    _ngUnsubscribe: Subject<any> = new Subject();
 
-    constructor( injector: Injector ) {
-        super(injector);
-        this.link = this._userParamsSetSrv.curentUser['ISN_LCLASSIF'];
-        this.selfLink = this._router.url.split('?')[0];
+    private btnDisable: boolean = true;
+    constructor(
+        private _userParamsSetSrv: UserParamsService,
+        private formHelp: FormHelperService,
+        private dataConv: EosDataConvertService,
+        private inpSrv: InputControlService,
+    ) {
+        this.allData = this._userParamsSetSrv.hashUserContext;
+    }
+    ngOnInit() {
+        this.pretInputs();
+        this.parseInputsFromString();
+        this.patchInputFuking();
+        this.form = this.inpSrv.toFormGroup(this.inputs);
+        this.formSubscriber();
+    }
+
+    pretInputs(): void {
+        this.prapareData = this.formHelp.parse_Create(CABINETS_USER.fields, this.allData);
+        this.prepareInputs = this.formHelp.getObjectInputFields(CABINETS_USER.fields);
+        this.inputs = this.dataConv.getInputs(this.prepareInputs, { rec: this.prapareData });
+    }
+
+    parseInputsFromString() {
+        this.fieldsKeys.forEach((val, key, arr) => {
+            this.inputs['rec.' + key].value = this.getValueForString(val, key);
+        });
+    }
+    getValueForString(val, key): boolean {
+        const str = this.allData['FOLDERCOLORSTATUS'];
+        return str.charAt(val) === '1' ? true : false;
+    }
+    patchInputFuking() {
+        if (String(this.inputs['rec.HILITE_PRJ_RC'].value).trim() !== '') {
+            this.inputs['rec.HILITE_PRJ_RC_BOOLEAN'].value = true;
+        } else {
+            this.inputs['rec.HILITE_PRJ_RC_BOOLEAN'].value = false;
+        }
+
+        if (String(this.inputs['rec.HILITE_RESOLUTION'].value).trim() !== '') {
+            this.inputs['rec.HILITE_RESOLUTION_BOOLEAN'].value = true;
+        } else {
+            this.inputs['rec.HILITE_RESOLUTION_BOOLEAN'].value = false;
+        }
+    }
+    formSubscriber() {
+        this.form.valueChanges.subscribe(data => {
+            this.checkTouch(data);
+        });
+    }
+
+    checkTouch(data) {
+        let countError = 0;
+        Object.keys(data).forEach(key => {
+            // if (key !== 'rec.HILITE_PRJ_RC_BOOLEAN'  && key !==  'rec.HILITE_RESOLUTION_BOOLEAN' && key !==  'rec.HILITE_RESOLUTION' && key !==  'rec.HILITE_PRJ_RC') {
+                if (this.inputs[key].value !== data[key]) {
+                    countError += 1;
+                    this.mapChanges.set(key, data[key]);
+                } else {
+                    if (this.mapChanges.has(key)) {
+                        this.mapChanges.delete(key);
+                    }
+                }
+        });
+        if (countError > 0 || this.mapChanges.size) {
+            this.btnDisable = false;
+        } else {
+            this.btnDisable = true;
+        }
+    }
+
+
+    checkDataToDisabled() {
+        const val = this.form.controls[`rec.HILITE_RESOLUTION_BOOLEAN`].value;
+        const val2 = this.form.controls[`rec.HILITE_PRJ_RC_BOOLEAN`].value;
+        if (!val) {
+            this.form.controls['rec.HILITE_RESOLUTION'].disable({emitEvent: false});
+        }   else {
+            this.form.controls['rec.HILITE_RESOLUTION'].enable({emitEvent: false});
+        }
+        if (!val2) {
+            this.form.controls['rec.HILITE_PRJ_RC'].disable({emitEvent: false});
+        }   else {
+            this.form.controls['rec.HILITE_PRJ_RC'].enable({emitEvent: false});
+        }
+    }
+    setTab(i: number) {
+        this.currTab = i;
+    }
+    edit(event) {
+
+    }
+    cancel(event?) {
+
     }
     ngOnDestroy() {
         this._ngUnsubscribe.next();
         this._ngUnsubscribe.complete();
     }
-    cancel($event?) {
-        this.flagEdit = false;
-        super.cancel();
-    }
-    edit($event) {
-        this.flagEdit = $event;
-        this.editMode();
-    }
-    close(event) {
-        this.flagEdit = event;
-        this._router.navigate(['user_param', JSON.parse(localStorage.getItem('lastNodeDue'))]);
-    }
+    // cancel($event?) {
+    //     this.flagEdit = false;
+    //     super.cancel();
+    // }
+    // edit($event) {
+    //     this.flagEdit = $event;
+    //     this.editMode();
+    // }
+    // close(event) {
+    //     this.flagEdit = event;
+    //     this._router.navigate(['user_param', JSON.parse(localStorage.getItem('lastNodeDue'))]);
+    // }
 }
