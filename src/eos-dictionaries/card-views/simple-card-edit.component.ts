bae7a0b67b695fe267474066271ffb38b8159a5b
hedgehog <derevenskihav@biz-it.ru>
2019-04-10 12:36:18 +0300

    fix Bug 100991:Основания задержаний. IE11.

--- src/eos-dictionaries/card-views/simple-card-edit.component.ts
+++ src/eos-dictionaries/card-views/simple-card-edit.component.ts
@@ -1,12 +1,22 @@
 import { Component, Injector } from '@angular/core';
 
 import { BaseCardEditComponent } from './base-card-edit.component';
+import { IDynamicInputOptions } from 'eos-common/dynamic-form-input/dynamic-input.component';
 
 @Component({
     selector: 'eos-simple-card-edit',
     templateUrl: 'simple-card-edit.component.html',
 })
+
 export class SimpleCardEditComponent extends BaseCardEditComponent {
+
+    selOpts: IDynamicInputOptions = {
+        defaultValue: {
+            value: '',
+            title: '...',
+        }
+    };
+
     constructor(injector: Injector) {
         super(injector);
     }
