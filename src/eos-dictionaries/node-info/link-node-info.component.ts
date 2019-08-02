8ca42672490c8efb5ee7501ceabeb2991b65eeec
hedgehog <derevenskihav@biz-it.ru>
2019-08-02 14:28:26 +0300

    Bug 103869: Типы связок 	Окно информации: 	- Блок «Прямая связка», содержащий следующие поля: Индекс, Наименование, чекбокс «Прозрачная связка», Примечание; 	- Блок «Обратная связка», содержащий следующие поля: Индекс, Наименование, чекбокс «Прозрачная связка», Примечание; 	- Категория.

---
+++ src/eos-dictionaries/node-info/link-node-info.component.ts
@@ -0,0 +1,15 @@
+import {Component, } from '@angular/core';
+import { NodeInfoComponent } from './node-info.component';
+
+@Component({
+    selector: 'eos-link-node-info',
+    templateUrl: 'link-node-info.component.html',
+})
+
+export class LinkNodeInfoComponent extends NodeInfoComponent {
+//      Блок «Прямая связка», содержащий следующие поля: Индекс, Наименование, чекбокс «Прозрачная связка», Примечание;
+//      Блок «Обратная связка», содержащий следующие поля: Индекс, Наименование, чекбокс «Прозрачная связка», Примечание;
+//      Категория.
+
+
+}
