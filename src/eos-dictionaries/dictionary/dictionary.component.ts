fb5029f5b1f0012244e9f581f8ca5fa4cdb50fe9
hedgehog <derevenskihav@biz-it.ru>
2019-11-06 14:01:57 +0300

    fix Bug 111377: не выдавать сообщение, если ни одна из записей не была удалена:

--- src/eos-dictionaries/dictionary/dictionary.component.ts
+++ src/eos-dictionaries/dictionary/dictionary.component.ts
@@ -762,7 +762,7 @@ export class DictionaryComponent implements OnDestroy, DoCheck, AfterViewInit {
                 return this._dictSrv.deleteMarked().then((results: IRecordOperationResult[]) => {
                     const deletedList = results.filter(r => !r.error)
                         .map ( r => r.record['CLASSIF_NAME']) ;
-                    if (deletedList) {
+                    if (deletedList && deletedList.length) {
                         const message: IMessage = Object.assign({}, INFO_OPERATION_COMPLETE);
                         message.msg = message.msg
                             .replace('{{RECS}}', deletedList.join(', '))
