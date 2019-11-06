af9646896ec1f28f329dc58d55734280139adfbd
hedgehog <derevenskihav@biz-it.ru>
2019-11-06 13:20:01 +0300

    bug/eos-110529-2: при смене сортировки "выбрано все" - не сбрасывается

--- src/eos-dictionaries/services/eos-dict.service.ts
+++ src/eos-dictionaries/services/eos-dict.service.ts
@@ -1431,10 +1431,10 @@ export class EosDictService {
 
             const page = this.paginationConfig;
             const pageList = this._visibleListNodes.slice((page.start - 1) * page.length, page.current * page.length);
-            /* unMark invisible nodes */
-            this._currentList
-                .filter((listNode) => listNode.isMarked && pageList.findIndex((pageNode) => pageNode.id === listNode.id) === -1)
-                .forEach((listNode) => listNode.isMarked = false);
+            // /* unMark invisible nodes */
+            // this._currentList
+            //     .filter((listNode) => listNode.isMarked && pageList.findIndex((pageNode) => pageNode.id === listNode.id) === -1)
+            //     .forEach((listNode) => listNode.isMarked = false);
 
             if (this._listNode && pageList.findIndex((node) => node.id === this._listNode.id) < 0) {
                 const firstMarkedIndex = pageList.findIndex((node) => node.isMarked);
