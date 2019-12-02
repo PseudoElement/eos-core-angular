7d4b62bbacbe84ab174640bb517c15b88d7140d9
Деревенских Александр Викторович <derevenskihav@biz-it.ru>
2019-12-02 13:15:26 +0300

    fix Bug 113595:ЦБ. Справочники. Ошибка при попытке добавить дублирующий корневой сертификат УЦ

--- src/eos-dictionaries/core/ca-category-dictionary-descriptor.ts
+++ src/eos-dictionaries/core/ca-category-dictionary-descriptor.ts
@@ -1,11 +1,43 @@
 import { DictionaryDescriptor } from './dictionary-descriptor';
 import { _ES } from 'eos-rest/core/consts';
 import { EDS_CATEGORY_CL } from 'eos-dictionaries/consts/dictionaries/category-eds.consts';
+import { ConfirmWindowService } from 'eos-common/confirm-window/confirm-window.service';
+import { IConfirmWindow2 } from 'eos-common/confirm-window/confirm-window2.component';
+import { BUTTON_RESULT_CANCEL } from 'app/consts/confirms.const';
+import { PipRX } from 'eos-rest/services/pipRX.service';
+import { CA_CATEGORY } from 'eos-rest/interfaces/structures';
 
 export class CaCategoryDictionaryDescriptor extends DictionaryDescriptor {
 
     getParentDictionaryId(): string {
         return EDS_CATEGORY_CL.id;
+    }
+
+    confirmSave(nodeData: any, confirmSrv: ConfirmWindowService, isNewRecord: boolean): Promise<boolean> {
+
+
+        return this.apiSrv.read<CA_CATEGORY>({'CA_CATEGORY': PipRX.criteries({
+            'CA_SERIAL': nodeData.rec.CA_SERIAL,
+            'CA_SUBJECT': nodeData.rec.CA_SUBJECT,
+            'ISN_EDS_CATEGORY': nodeData.rec.ISN_EDS_CATEGORY}), })
+            .then((data) => {
+                if (data && data. length) {
+                    const uniqueMessage: IConfirmWindow2 = {
+                        title: 'Не удалось сохранить',
+                        body: 'Корневой сертификат уже был добавлен ранее',
+                        buttons: [{
+                            title: 'ОК',
+                            result: BUTTON_RESULT_CANCEL,
+                            isDefault: true,
+                        }, ],
+                    };
+                    return confirmSrv.confirm2(uniqueMessage).then( (button) => {
+                        return false;
+                    });
+                }
+                return true;
+            });
+
     }
 
 }
