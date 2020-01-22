edbb1bade19e42686534d654fabe96ccf73e9c98
vidnik.andrei <andrei.vidnik@mrsoft.by>
2020-01-22 14:23:39 +0300

    bug 115189 - ЦБ. Пользователи. Системный технолог с ограниченным доступом к справочнику "Пользователи" не может редактировать пользователя, подразделение которого принадлежит доступной ему картотеке

--- src/eos-rest/services/appContext.service.ts
+++ src/eos-rest/services/appContext.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { PipRX } from './pipRX.service';
-import { USER_CL, SYS_PARMS, SRCH_VIEW, USER_TECH } from '../interfaces/structures';
+import { USER_CL, SYS_PARMS, SRCH_VIEW } from '../interfaces/structures';
 import { ALL_ROWS } from '../core/consts';
 import { Deferred } from '../core/pipe-utils';
 import { IUserParms } from 'eos-rest';
@@ -38,7 +38,7 @@ export class AppContext {
         const ua = window.navigator.userAgent;
         return /MSIE|Trident/.test(ua);
     }
-    updateLimitCardsUser(newLimitCard: USER_TECH[]) {
+    updateLimitCardsUser(newLimitCard: Array<string>) {
         this.limitCardsUser = newLimitCard;
     }
 
