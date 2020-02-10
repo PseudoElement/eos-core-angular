64bf55b3e50ce1d048eea13386d7994f25265ddd
Деревенских Александр Викторович <derevenskihav@biz-it.ru>
2020-02-10 14:48:26 +0300

    fix Bug 117138:Справочники. Категории ЭП. 	"Наименование" длиной 64 символа, а "Примечание" - 255 символов, как в толстяке.

--- src/eos-dictionaries/consts/dictionaries/category-eds.consts.ts
+++ src/eos-dictionaries/consts/dictionaries/category-eds.consts.ts
@@ -1,6 +1,6 @@
 import { IDictionaryDescriptor } from 'eos-dictionaries/interfaces';
 import { LINEAR_TEMPLATE } from './_linear-template';
-import { COMMON_FIELD_NAME } from './_common';
+import { COMMON_FIELD_NAME, COMMON_FIELD_NOTE } from './_common';
 
 export const EDS_CATEGORY_CL: IDictionaryDescriptor = Object.assign({}, LINEAR_TEMPLATE, {
     id: 'eds-category',
@@ -10,10 +10,14 @@ export const EDS_CATEGORY_CL: IDictionaryDescriptor = Object.assign({}, LINEAR_T
     visible: true,
     iconName: 'eos-icon-electronic-signature-blue',
     fields: [...LINEAR_TEMPLATE.fields,
-        Object.assign({}, COMMON_FIELD_NAME, {
-            isUnique: true,
-            uniqueInDict: true,
-        })],
+    Object.assign({}, COMMON_FIELD_NAME, {
+        isUnique: true,
+        uniqueInDict: true,
+        length: 64,
+    }),
+    Object.assign({}, COMMON_FIELD_NOTE, {
+        length: 255,
+    })],
     quickViewFields: ['CLASSIF_NAME', 'NOTE'],  // CLASSIF_NAME is in shortQuickViewFields
     listFields: ['CLASSIF_NAME', 'NOTE'],
     allVisibleFields: [],
