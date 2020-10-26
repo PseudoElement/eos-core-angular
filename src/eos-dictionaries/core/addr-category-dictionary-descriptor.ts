0c073e7a0e07da245b1cb568375df467dab09aca
Видник Андрей Владимирович <andrei.vidnik@mrsoft.by>
2020-10-26 16:56:01 +0300

    from prod/19.6 to dev/20.2 #131622 Справочники. Ошибка при быстром поиске в Категориях адресатов

--- src/eos-dictionaries/core/addr-category-dictionary-descriptor.ts
+++ src/eos-dictionaries/core/addr-category-dictionary-descriptor.ts
@@ -4,7 +4,7 @@ import { _ES } from 'eos-rest/core/consts';
 export class AddrCategoryDictionaryDescriptor extends DictionaryDescriptor {
 
     getData(...params): Promise<any[]> {
-        return super.getData(params).then ( data => {
+        return super.getData(params.length ? params[0] : []).then ( data => {
             return data.filter(d => d.ISN_LCLASSIF && d.ISN_LCLASSIF > 0);
         });
     }
