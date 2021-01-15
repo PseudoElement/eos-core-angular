2de6e6073710db9f69435a7a489bcad05a936de6
Акулович Виталий Леонидович <vitali.akulovich@mrsoft.by>
2021-01-15 17:18:25 +0300

    from dev/19.6 to dev/20.2 Bug 135999: Номенклатура дел. ограниченному технологу доступка кнопка "создать" в "чужом" подразделении, если в результате поиска была найдена номеклатура из своего

--- src/eos-dictionaries/node-actions/node-actions.component.ts
+++ src/eos-dictionaries/node-actions/node-actions.component.ts
@@ -189,6 +189,8 @@ export class NodeActionsComponent implements OnDestroy {
         if (!this.dictionary) {
             return null;
         }
+        // если в режиме поиска в Номенклатуре, то смотреть на вершину дерева
+        let nomenklListSearch = null;
 
         const marklist = this._markedNodes;
         let listHasDeleted = null;
@@ -196,11 +198,13 @@ export class NodeActionsComponent implements OnDestroy {
         if (this.dictionary && this._viewParams && this._dictSrv) {
             listHasDeleted = marklist.filter(n => n.isDeleted).length !== 0;
             listHasSelected = marklist.length !== 0;
+            nomenklListSearch = listHasSelected && this.dictionary.id === 'nomenkl' &&
+                this._viewParams.searchResults;
         }
 
         let grant = APS_DICT_GRANT.denied;
 
-        if (marklist && marklist.length > 0 /* && (this._dictSrv.isSearchFullDictionary() || this._viewParams.showAllSubnodes) */) {
+        if (marklist && marklist.length > 0 && !nomenklListSearch /* && (this._dictSrv.isSearchFullDictionary() || this._viewParams.showAllSubnodes) */) {
             let due = null;
             let all_grant = APS_DICT_GRANT.readwrite;
             for (let i = 0; i < marklist.length; i++) {
