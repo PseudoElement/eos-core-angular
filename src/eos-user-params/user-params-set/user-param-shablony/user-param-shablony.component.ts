f341ae2a431213cac7c916027ebe873f797d70cd
Видник Андрей Владимирович <andrei.vidnik@mrsoft.by>
2021-02-22 17:22:30 +0300

    bug #138770 b.1 В настройках по умолчанию на вкладке шаблоны убрать из имени шаблона имя файла

--- src/eos-user-params/user-params-set/user-param-shablony/user-param-shablony.component.ts
+++ src/eos-user-params/user-params-set/user-param-shablony/user-param-shablony.component.ts
@@ -14,6 +14,7 @@ import { ErrorHelperServices } from 'eos-user-params/shared/services/helper-erro
 @Component({
     selector: 'eos-user-param-shablony',
     templateUrl: 'user-param-shablony.component.html',
+    styleUrls: ['user-param-shablony.component.scss'],
     providers: [FormHelperService],
 })
 
