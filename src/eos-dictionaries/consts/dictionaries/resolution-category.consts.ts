c8b22386a1ebdc497e67dc1ddfeda86df6eb2c19
maksim vashkevich <onpustmacs@yandex.by>
2021-06-10 17:21:41 +0300

    144262 В справочнике Категория оручений нет поля Срок

--- src/eos-dictionaries/consts/dictionaries/resolution-category.consts.ts
+++ src/eos-dictionaries/consts/dictionaries/resolution-category.consts.ts
@@ -21,16 +21,51 @@ export const RESOL_CATEGORY_DICT: IDictionaryDescriptor = Object.assign({}, LINE
             title: 'Индекс СЭВ',
             type: 'dictionary',
             isUnique: true,
-        }
+        },
+        {
+            key: 'PLAN_COUNT_FLAG',
+            type: 'boolean',
+            length: 10,
+            title: 'Использовать для расчёта плановой даты поручения',
+        },
+        {
+            key: 'PLAN_DAY_COUNT',
+            page: 'D',
+            type: 'numberIncrement',
+            // kind_doc '1,2,3',
+            title: 'Срок',
+            pattern: /(^\d{1,4}$)/,
+            minValue: 0,
+            maxValue: 9999,
+            order: 30,
+            default: 0,
+        },
+        {
+            key: 'PLAN_DAY_TYPE',
+            type: 'select',
+            title: '',
+            default: 2,
+            options: [
+                { value: 1, title: 'календарн.' },
+                { value: 2, title: 'рабоч.' },
+            ],
+        },
+        {
+            key: 'PLAN_PROTECTED',
+            type: 'boolean',
+            length: 10,
+            title: 'Запретить редактирование плановой даты',
+        },
     ],
     listFields: [
         ... Features.cfg.SEV.isIndexesEnable ? [ICONS_CONTAINER_SEV] : [],
         'CLASSIF_NAME'],
     editFields: [ ... LINEAR_TEMPLATE.editFields,
         ... Features.cfg.SEV.isIndexesEnable ? ['sev'] : [],
+        'PLAN_COUNT_FLAG', 'PLAN_DAY_COUNT', 'PLAN_DAY_TYPE', 'PLAN_PROTECTED',
     ],
     quickViewFields: ['CLASSIF_NAME',
-        ... Features.cfg.SEV.isIndexesEnable ? ['sev'] : [],
+        ... Features.cfg.SEV.isIndexesEnable ? ['sev'] : [], 'PLAN_COUNT_FLAG', 'PLAN_PROTECTED',
     ],
 
 });
