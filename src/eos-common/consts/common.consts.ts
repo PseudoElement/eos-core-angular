271ba98182755502f6a18bccd2d6ffac4591bf96
maksim vashkevich <onpustmacs@yandex.by>
2021-09-01 17:11:39 +0300

    #147298, добавил некоторые елементы пунктуации в разрешённые

--- src/eos-common/consts/common.consts.ts
+++ src/eos-common/consts/common.consts.ts
@@ -12,7 +12,7 @@ export const NUMERIC_PATTERN = /(^\d{1,8}$)/;
 // export const VALID_REQ_STRING = /^(?!\ *$)[\wА-Яа-я.+\ \'\-]+[^\:\"\|!@$&^=~]*$/;
 export const VALID_REQ_STRING = /[^\s]+/;
 export const NOT_EMPTY_STRING2 = /[^\s]+/;
-export const VALID_REQ_MULTIPLE_STRING = /^(?!\s*$)[\wА-Яа-я.+\s\'\-]+$/;
+export const VALID_REQ_MULTIPLE_STRING = /^(?!\s*$)[\wА-Яа-я.+\s:;,\'\-]+$/;
 export const INPUT_ERROR_MESSAGES = {
     required: 'Обязательное поле.',
     isUnique: [
