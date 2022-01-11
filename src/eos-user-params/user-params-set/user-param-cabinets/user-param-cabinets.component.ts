c807baddab942ef5db5bbac0533b23668ab0c35b
maksim vashkevich <onpustmacs@yandex.by>
2022-01-11 12:02:48 +0300

    #152860, Иконка лог. удаления ДЛ не двигается вместе с полосой прокрутки

--- src/eos-user-params/user-params-set/user-param-cabinets/user-param-cabinets.component.ts
+++ src/eos-user-params/user-params-set/user-param-cabinets/user-param-cabinets.component.ts
@@ -164,7 +164,11 @@ export class UserParamCabinetsComponent implements OnDestroy, OnInit {
         this.editMode();
         this.formSubscriber();
         return Promise.all([this.getControlAuthor(), this.getNameSortCabinets()]).then(([author, sort]) => {
-            this.logDeletController = Boolean(author[0]['DELETED']);
+            if (author && author[0]) {
+                this.logDeletController = Boolean(author[0]['DELETED']);
+            } else {
+                this.logDeletController = false;
+            }
             [...CABINETS_USER_FOLDERS.fields, ...CABINETS_USER_ASSIGMENTS.fields].map(fields => {
                 if (fields.key === 'CABSORT_ISN_DOCGROUP_LIST') {
                     fields.options.splice(1, fields.options.length);
@@ -398,7 +402,11 @@ export class UserParamCabinetsComponent implements OnDestroy, OnInit {
                     this.form.controls['rec.RESOLUTION_CONTROLLER'].patchValue(String(this.dueForLink));
                     this.form.controls['rec.CONTROLL_AUTHOR'].patchValue(String(data[0]['CLASSIF_NAME']));
                     this.controller = true;
-                    this.logDeletController = Boolean(data[0]['DELETED']);
+                    if (Array.isArray(data) && data.length > 0) {
+                        this.logDeletController = Boolean(data[0]['DELETED']);
+                    } else if (typeof(data) === 'object') {
+                        this.logDeletController = Boolean(data['DELETED']);
+                    }
                 }
             })
             .catch(error => {
