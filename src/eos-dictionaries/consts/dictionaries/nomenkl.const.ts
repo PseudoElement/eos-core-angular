b396583c7ccdab925aefcc0398781e749d24ab85
maksim vashkevich <onpustmacs@yandex.by>
2022-01-27 17:48:27 +0300

    from prod/20.4 to dev/21.1 153478, Как вызвать протокол в справочнике Номенклатура дел?

--- src/eos-dictionaries/consts/dictionaries/nomenkl.const.ts
+++ src/eos-dictionaries/consts/dictionaries/nomenkl.const.ts
@@ -1,7 +1,7 @@
-import {E_DICT_TYPE, IDictionaryDescriptor, IFieldPreferences} from 'eos-dictionaries/interfaces';
-import {LINEAR_TEMPLATE} from './_linear-template';
-import {COMMON_FIELD_ICONS, COMMON_FIELD_NAME} from './_common';
-import {YEAR_PATTERN/* , VALID_REQ_MULTIPLE_STRING */} from 'eos-common/consts/common.consts';
+import { E_DICT_TYPE, IDictionaryDescriptor, IFieldPreferences } from 'eos-dictionaries/interfaces';
+import { LINEAR_TEMPLATE } from './_linear-template';
+import { COMMON_FIELD_ICONS, COMMON_FIELD_NAME } from './_common';
+import { YEAR_PATTERN/* , VALID_REQ_MULTIPLE_STRING */ } from 'eos-common/consts/common.consts';
 import { SECURITY_DICT } from './security.consts';
 
 export const DID_NOMENKL_CL = 'nomenkl';
@@ -12,10 +12,11 @@ export const NOMENKL_DICT: IDictionaryDescriptor = Object.assign({}, LINEAR_TEMP
     actions: [
         'add', 'markRecords', 'quickSearch', 'fullSearch', 'order', 'uniqueIndexDel', 'userOrder', 'printNomenc',
         'moveUp', 'moveDown', 'navigateUp', 'navigateDown', 'tableCustomization', 'removeHard',
-        'edit', 'view',  'userOrder', 'userOrder', 'export', 'import', 'copyNodes', 'pasteNodes',
+        'edit', 'view', 'userOrder', 'userOrder', 'export', 'import', 'copyNodes', 'pasteNodes',
         'OpenSelected',
         'CloseSelected',
-        'showDeleted'
+        'showDeleted',
+        'protViewSecurity'
     ],
     defaultOrder: 'NOM_NUMBER',
     visible: true,
@@ -126,37 +127,37 @@ export const NOMENKL_DICT: IDictionaryDescriptor = Object.assign({}, LINEAR_TEMP
         type: 'boolean',
         required: true,
         default: 0,
-    // }, {
-    //     key: 'buttPer',
-    //     type: 'buttons',
-    //     title: '',
-    //     length: 100,
-    //     options: [{value: 0, title: 'Текущее'}, {value: 1, title: 'Переходящее'}],
-    //     default: null,
-    //     // isNoDBInput: true,
+        // }, {
+        //     key: 'buttPer',
+        //     type: 'buttons',
+        //     title: '',
+        //     length: 100,
+        //     options: [{value: 0, title: 'Текущее'}, {value: 1, title: 'Переходящее'}],
+        //     default: null,
+        //     // isNoDBInput: true,
     },
         COMMON_FIELD_ICONS,
-        Object.assign({}, COMMON_FIELD_NAME, {
-            title: 'Заголовок',
-            length: 2000,
-            type: 'text',
-            // pattern: VALID_REQ_MULTIPLE_STRING,
-            preferences: <IFieldPreferences>{
-                minColumnWidth: 250,
-            }
-        }),
+    Object.assign({}, COMMON_FIELD_NAME, {
+        title: 'Заголовок',
+        length: 2000,
+        type: 'text',
+        // pattern: VALID_REQ_MULTIPLE_STRING,
+        preferences: <IFieldPreferences>{
+            minColumnWidth: 250,
+        }
+    }),
     ]),
 
     treeFields: ['CLASSIF_NAME'],
     editFields: ['CLASSIF_NAME', 'NOTE', 'CLOSED', 'SECURITY', 'YEAR_NUMBER', 'SHELF_LIFE', 'NOM_NUMBER',
         'END_YEAR', 'ARTICLE', 'ARCH_DATE', 'ARCH_FLAG', 'E_DOCUMENT', 'buttPer'],
-    searchFields: ['CLASSIF_NAME', 'NOM_NUMBER', ],
+    searchFields: ['CLASSIF_NAME', 'NOM_NUMBER'],
     fullSearchFields: ['CLASSIF_NAME', 'NOTE', 'NOM_NUMBER'],
-    quickViewFields: ['NOM_NUMBER', 'SHELF_LIFE', 'SECURITY', 'E_DOCUMENT', 'CLOSED',  'YEAR_NUMBER',
-    'END_YEAR', 'ARTICLE', 'CLOSE_WHO', 'CLOSE_DATE', 'ARCH_FLAG', 'ARCH_DATE', 'NOTE'],
+    quickViewFields: ['NOM_NUMBER', 'SHELF_LIFE', 'SECURITY', 'E_DOCUMENT', 'CLOSED', 'YEAR_NUMBER',
+        'END_YEAR', 'ARTICLE', 'CLOSE_WHO', 'CLOSE_DATE', 'ARCH_FLAG', 'ARCH_DATE', 'NOTE'],
     shortQuickViewFields: ['CLASSIF_NAME'],
-    listFields: ['ICONS_TYPE', 'NOM_NUMBER', 'CLASSIF_NAME', ],
+    listFields: ['ICONS_TYPE', 'NOM_NUMBER', 'CLASSIF_NAME'],
     allVisibleFields: ['NOTE', 'CLOSED', 'SECURITY', 'SHELF_LIFE', 'YEAR_NUMBER',
-        'END_YEAR', 'ARTICLE', 'CLOSE_WHO', 'CLOSE_DATE', 'ARCH_DATE', 'ARCH_FLAG', 'E_DOCUMENT'/* , 'ICONS_TYPE' */ ],
+        'END_YEAR', 'ARTICLE', 'CLOSE_WHO', 'CLOSE_DATE', 'ARCH_DATE', 'ARCH_FLAG', 'E_DOCUMENT'/* , 'ICONS_TYPE' */],
 
 });
