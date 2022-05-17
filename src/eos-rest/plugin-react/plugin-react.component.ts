d9e10965c942ce0208327272a3d7c912892c3008
mrAndrei <andreividnik59@gmail.com>
2022-05-17 16:16:53 +0300

    task #8896  Окно "Текущие сессии пользователей"

--- src/eos-rest/plugin-react/plugin-react.component.ts
+++ src/eos-rest/plugin-react/plugin-react.component.ts
@@ -1,6 +1,5 @@
 import { DOCUMENT } from '@angular/common';
-import { AfterViewInit, Component, Inject, OnDestroy, OnInit, Renderer2, SecurityContext } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
+import { AfterViewInit, Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import { NavParamService } from 'app/services/nav-param.service';
 import { AppContext } from 'eos-rest/services/appContext.service';
 
@@ -12,7 +11,7 @@ import { AppContext } from 'eos-rest/services/appContext.service';
 export class PluginReactComponent implements OnInit, AfterViewInit, OnDestroy {
     scriptsUrl: any;
 
-    constructor(private sanitizer: DomSanitizer, private render: Renderer2, @Inject(DOCUMENT) private document: Document, private _navSrv: NavParamService, private appCtx: AppContext) {
+    constructor( @Inject(DOCUMENT) private document: Document, private _navSrv: NavParamService, private appCtx: AppContext) {
       window['uisn'] = this.appCtx.CurrentUser.ISN_LCLASSIF;
     }
 
@@ -22,26 +21,16 @@ export class PluginReactComponent implements OnInit, AfterViewInit, OnDestroy {
       });
     }
     ngAfterViewInit(): void {
-      this.scriptsUrl = this.sanitizer.sanitize(SecurityContext.URL, '../UserSessionPlugin/load-plugin-nocache.js');
-      const script = this.render.createElement('script');
-      script.src = this.scriptsUrl;
-      script.async = true;
-      const div_placer = this.document.getElementById('plugins_place_session');
-      this.render.appendChild(div_placer, script);
+      const w: any = window;
+      w.pluginManager.loadPlugins();
     }
 
     ngOnDestroy(): void {
-      const div_placer = this.document.getElementById('plugins_place_session');
       const header = this.document.getElementsByTagName('head');
       if (header[0]) {
         if (/\.App/.test(header[0].lastChild.textContent)) {
             header[0].removeChild(header[0].lastChild);
         }
-
-      }
-      if (div_placer.firstChild) {
-        div_placer.removeChild(div_placer.firstChild);
-        div_placer.removeChild(div_placer.lastChild);
       }
     }
 }
