6539a41b5315f4f45f5b10415a03e1c555c0042b
Vidnik Andrei <andrei.vidnik@mrsoft.by>
2022-12-01 16:54:31 +0300

    Заглушка на ломанные плагины.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { Manager } from '@eos/jsplugins-manager';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 if (environment.production) {
     enableProdMode();
 }
 
-function bootstrap(event: unknown) {
-    platformBrowserDynamic().bootstrapModule(AppModule);
-    window.removeEventListener('error', bootstrap);
-}
+platformBrowserDynamic().bootstrapModule(AppModule);
 
-window.addEventListener('error', bootstrap);
-
-Manager.loadPlugins({ targets: ['tech_tasks', 'DictionariesMetadata'] }).then(() => {
-    platformBrowserDynamic().bootstrapModule(AppModule);
-});
