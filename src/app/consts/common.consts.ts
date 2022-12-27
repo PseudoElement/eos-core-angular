158661bbb8433707a304769c0a5c1778ee50916f
maksim vashkevich <onpustmacs@yandex.by>
2022-12-27 15:19:14 +0300

    #167098, правлю url финальное исправление

--- src/app/consts/common.consts.ts
+++ src/app/consts/common.consts.ts
@@ -1,5 +1,5 @@
 export const RECENT_URL = 'recentUrl';
 export const LS_EDIT_CARD = 'lastEditedCard';
 export const STORAGE_WEIGHTORDER = 'storageWeightOrder';
-export const URL_LOGIN: string = '../Auth/Login';
+export const URL_LOGIN: string = '../login/';
 
