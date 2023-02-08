bdc85d993cbb862574645fcc7ba79165bf34680d
Ihar Novikau <gosha.noviko2012@gmail.com>
2023-02-08 17:27:23 +0300

    #168580, создал кастомный компонент для отображения broadcast-channel

---
+++ src/eos-dictionaries/node-info/broadcast-channel-node-info.component.ts
@@ -0,0 +1,8 @@
+import {Component, } from '@angular/core';
+import { NodeInfoComponent } from './node-info.component';
+@Component({
+    selector: 'eos-sev-broadcast-channel-node-info',
+    templateUrl: 'broadcast-channel-node-info.component.html',
+})
+
+export class BroadcastChannelNodeInfoComponent extends NodeInfoComponent {}
