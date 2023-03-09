ce1201ef4a7c5ba4dce52d34ee250ddc574c5ad7
maksim vashkevich <onpustmacs@yandex.by>
2023-03-09 09:02:43 +0300

    #168024, добавил регистрацию плагина в билде

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 
 // File "destination.txt" will be created or overwritten by default.
-fs.copyFile('./scripts/regMainMenu.js', './dist/regMainMenu.js', (err) => {
+fs.copyFile('./scripts/regMainMenu.js', './dist/box/regMainMenu.js', (err) => {
     if (err)
         throw err;
-    console.log('regMainMenu.js was copied to ./dist/regMainMenu.js');
+    console.log('regMainMenu.js was copied to ./dist/box/regMainMenu.js');
 });
-fs.copyFile('./scripts/plugin.json', './dist/plugin.json', (err) => {
+fs.copyFile('./scripts/plugin.json', './dist/box/plugin.json', (err) => {
     if (err)
         throw err;
-    console.log('plugin.json was copied to ./dist/plugin.json');
+    console.log('plugin.json was copied to ./dist/box/plugin.json');
 });
