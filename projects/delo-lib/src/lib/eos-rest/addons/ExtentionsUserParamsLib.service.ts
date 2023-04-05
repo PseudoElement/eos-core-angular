5af63b524d9e521593781ab76f2ac4fb1c978442
Ihar Novikau <gosha.noviko2012@gmail.com>
2023-04-05 15:06:01 +0300

    #169650, task:  Добавил полный путь к подразделению в подробной информации для ДЛ. Добавил отображение вышестоящее подразделение в справочнике "Подразделения".

--- projects/delo-lib/src/lib/eos-rest/addons/ExtentionsUserParamsLib.service.ts
+++ projects/delo-lib/src/lib/eos-rest/addons/ExtentionsUserParamsLib.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import { PipRX } from '../../eos-rest/services/pipRX.service';
+import { DEPARTMENT } from '../../eos-rest/interfaces/structures';
 @Injectable()
 export class ExetentionsUserParamsLib {
-    constructor() { }
-    public loadDepartments(value): Promise<any> {
-        return Promise.resolve(null);
+    NOTE: string;
+    constructor(
+        private pipRX: PipRX,
+    ) {}
+    public async loadDepartments(value: string): Promise<any> {
+        this.NOTE = '';
+        let due = value.split('.').map(el => el += '.');
+        due.pop();
+        due.pop();
+        const resultDue = [];
+        let curentDue:string = '';
+        due.forEach(el => {
+            curentDue += el
+            resultDue.push(curentDue)
+        })
+        const result: DEPARTMENT[] = await this.pipRX.read({ DEPARTMENT: {criteries: {DUE: resultDue.join('|')}}});
+        result.forEach( el => {
+            if (el.CARD_NAME === "Центральная картотека") {
+                this.NOTE += el.CARD_NAME;
+            } else {
+                this.NOTE += ' - ' + el.CLASSIF_NAME;
+            }
+        })
+        return this.NOTE;
     }
 }
