e0d14d6fa1f2701e1b4e075d8028d8b94afd021e
maksim vashkevich <onpustmacs@yandex.by>
2023-04-13 14:56:47 +0300

    #167916, Не всегда прорисовывается плитка Управление печатными формами

--- projects/box/src/main.ts
+++ projects/box/src/main.ts
@@ -8,18 +8,21 @@ import { environment } from './environments/environment';
 if (environment.production) {
     enableProdMode();
 }
+
 /* заглушка на загрузку плагинов , убрать когда плагины переведут на новый сборщик */
 try {
-    Manager.loadPlugins({ targets: ['tech_tasks', 'tech_tools'] }).then(() => {
+
+    (async () => {
+        await Manager.loadPlugins({ targets: ['tech_tasks', 'tech_tools'] })
         onlyOneDinamic();
-    });
+    })()
 } catch (error) {
-    onlyOneDinamic();
     console.log('err', error);
+    onlyOneDinamic();
 }
 
 function onlyOneDinamic() {
     platformBrowserDynamic().bootstrapModule(AppModule);
 }
 
-onlyOneDinamic()
+// onlyOneDinamic()
