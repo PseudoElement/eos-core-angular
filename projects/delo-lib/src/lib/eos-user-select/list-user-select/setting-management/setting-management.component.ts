0d86bbb04900da9c6f038e6f8e44649b0ff9c54b
Maksim Vashkevich <onpustmacs@yandex.by>
2023-08-02 09:29:24 +0300

    #173590, изменил номер элемента для копирования прав пользователя

--- projects/delo-lib/src/lib/eos-user-select/list-user-select/setting-management/setting-management.component.ts
+++ projects/delo-lib/src/lib/eos-user-select/list-user-select/setting-management/setting-management.component.ts
@@ -407,7 +407,7 @@ export class SettingManagementComponent implements OnInit, OnDestroy {
                 this.form.controls['3'].patchValue(value, { emitEvent: false });
                 this.form.controls['5'].patchValue(value, { emitEvent: false });
                 // this.form.controls['6'].patchValue(value, { emitEvent: false });
-                this.form.controls['7'].patchValue(value, { emitEvent: false });
+                this.form.controls['6'].patchValue(value, { emitEvent: false });
                 this.form.controls['8'].patchValue(value, { emitEvent: true });
                 this.form.controls['1'].patchValue(value, { emitEvent: false });
             }
