5faecbd368f302ee60ba6b03ed2c3cda2042ca2c
Ihar Novikau <i.novikov@mrsoft.by>
2023-08-14 16:56:08 +0300

    #174026, refactor: Адаптировал метод под глобаьную константу.

--- projects/delo-lib/src/lib/eos-dictionaries/services/creator-graphQl-param/protName-graphQl-param.ts
+++ projects/delo-lib/src/lib/eos-dictionaries/services/creator-graphQl-param/protName-graphQl-param.ts
@@ -2,8 +2,8 @@ import { AbstratCreatorGraphQlParam } from "./abstract-creator-graphQl-param";
 
 export class ProtNameGraphQlParam extends AbstratCreatorGraphQlParam {
 
-    public protName(param: string[]) {
-        const queryParam = this.createParamInString(param);
+    public protName(param: string) {
+        const queryParam = `{value: "${param}"}`;
         return ` protNamesPg(filter: {tableId: {in: [${queryParam}]}}, first: 1000, orderby: {describtion: Asc}) {
             items {
                 describtion
