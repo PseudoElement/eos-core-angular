ca287af6e2fce814272870c1a02e08299dd62583
Ihar Novikau <i.novikov@mrsoft.by>
2023-08-14 16:30:53 +0300

    #174026, task: Создал enum.

---
+++ projects/delo-lib/src/lib/eos-dictionaries/consts/dictionaries/enum/dictionaryId.enum.ts
@@ -0,0 +1,84 @@
+export enum E_DICTIONARY_ID {
+    /** Линейный справочник */
+    LINEAR_TEMPLATE = '',
+    /** Категории адресатов */
+    ADDR_CATEGORY = 'addr-category',
+    /** Корневые сертификаты УЦ */
+    CA_CATEGORY_CL = 'ca-category',
+    /** Кабинеты */
+    CABINET = 'cabinet',
+    /** Ведение календаря */
+    CALENDAR = 'calendar',
+    /** Категории ЭП */
+    EDS_CATEGORY_CL = 'eds-category',
+    /** Граждане */
+    CITIZENS = 'citizens',
+    /** Статус заявителя */
+    CITSTATUS = 'cistatus',
+    /** Контакты */
+    CONTACT = 'contact',
+    /** Виды доставки */
+    DELIVERY = 'delivery',
+    /** Подразделения */
+    DEPARTMENTS = 'departments',
+    /** Группы документов */
+    DOCGROUP = 'docgroup',
+    /** Категории файлов */
+    FILE_CATEGORIES = 'file-category',
+    /** Типы файлов */
+    FILE_TYPE = 'filetype',
+    /** Форматы сохранения */
+    FORMAT = 'format',
+    /** Типы связок */
+    LINK = 'link',
+    /** Узлы МЭДО */
+    MEDO_NODE = 'medo-node-cl',
+    /** Номенклатура дел */
+    DID_NOMENKL_CL = 'nomenkl',
+    /** Типы организаций */
+    ORG_TYPE = 'org-type',
+    /** Организации */
+    ORGANIZ = 'organization',
+    /** printInfo */
+    PRINT_INFO = 'printInfo',
+    /** Типы реестров */
+    REESTRTYPE = 'reestrtype',
+    /** Регионы */
+    REGION = 'region',
+    /** Категории поручений */
+    RESOL_CATEGORY = 'resolution-category',
+    /** Приоритеты проектов резолюций */
+    RESPRJ_PRIORITY = 'reprj-priority',
+    /** Статусы проекта поручения */
+    RESPRJ_STATUS = 'reprj-status',
+    /** Рубрикатор */
+    RUBRICATOR = 'rubricator',
+    /** Грифы доступа */
+    SECURITY = 'security',
+    /** Виды подписей */
+    SIGN_KIND = 'sign-kind',
+    /** Состояния исполнения (поручение) */
+    STATUS_EXEC = 'status-exec',
+    /** Состояния исполнения (исполнитель) */
+    STATUS_REPLY = 'status-reply',
+    /** Настройка шаблонов */
+    TEMPLATES = 'templates',
+    /** Типы адреса */
+    ADDRESS_VID_CL = 'address-vid',
+    /** Виды документов */
+    TYPE_DOCUM = 'type-docum',
+    /** Типы виз */
+    VISA_TYPE = 'visa-type',
+    /** Каналы передачи сообщений */
+    BROADCAST_CHANNEL = 'broadcast-channel',
+    /** Правила СЭВ */
+    RULES_SEV = 'sev-rules',
+    /** Коллизии СЭВ */
+    COLLISIONS_SEV = 'sev-collisions',
+    /** Участники СЭВ */
+    PARTICIPANT_SEV = 'sev-participant',
+    /** Справочники СЭВ */
+    SEV_FOLDER = 'SEV',
+    /** Индексы СЭВ */
+    SEV_ASSOCIATION = 'sev-association'
+}
