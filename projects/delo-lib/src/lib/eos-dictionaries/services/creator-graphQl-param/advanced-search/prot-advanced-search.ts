67e085244628e0d4dc2a8d4eefb060eed7e7588e
Ihar Novikau <i.novikov@mrsoft.by>
2023-08-18 14:23:04 +0300

    #174176, bug: Переработал поиск по протоколу.

--- projects/delo-lib/src/lib/eos-dictionaries/services/creator-graphQl-param/advanced-search/prot-advanced-search.ts
+++ projects/delo-lib/src/lib/eos-dictionaries/services/creator-graphQl-param/advanced-search/prot-advanced-search.ts
@@ -1,37 +1,37 @@
+import { PROTOCOL_ID } from '../../../../eos-dictionaries/consts/protocolId.const';
 import { Protocol } from '../../../interfaces/fetch.interface';
 import { AbstractAdvancedSearch } from './abstract-advanced-search';
 
 export class ProtAdvancedSearch extends AbstractAdvancedSearch{
 
-    public prot(param: Protocol): string {
+    public prot(param: Protocol, dictId: string): string {
         let operDescribeper: string = '';
         let operDescribeperSearchParam: string = '';
-        
-        if(param.OPER_DESCRIBE) {
-            const oper: string[] = param.OPER_DESCRIBE.split('|');
+        const table_ID: string = PROTOCOL_ID[dictId];
+
+        console.log({param})
+        if(param.OPERATION && param.OPERATION.length) {
+            const oper: string[] = [];
+            param.OPERATION.forEach(el => {
+                oper.push(el.describtion)
+            })
             operDescribeper = this.createParamInString(oper);
             operDescribeperSearchParam = `in: [${operDescribeper}]`;
         }
-
-        return `protsPg(first: 1000000,filter: {
-                    userIsn: {equal: {value: ${param.USER_ISN}}}, 
-                    operDescribe: {${operDescribeperSearchParam}}, 
-                    timeStamp: {
-                        greaterOrEqual: {value: "${param.FROM}"}, 
-                        lessOrEqual: {value: "${param.TO}"}
-                    }
-                }) {
-                    items {
-                        suboperId
-                        operDescribe
-                        operComment
-                        isnProtInfo
-                        userIsn
-                        timeStamp
-                        tableId
-                        refIsn
-                        operId
-                    }
-                }`;
+        return `protsPg(first: 1000000, filter: {
+            userIsn: {equal: {value: ${param.USER_ISN}}}, 
+            tableId: {contains: {value: "${table_ID}"}},
+            protName: {matches: {describtion: {
+                ${operDescribeperSearchParam}
+            }}}
+            timeStamp: {
+                greaterOrEqual: {value: "${param.FROM}"}, 
+                lessOrEqual: {value: "${param.TO}"}
+            }
+        }) {
+            items {
+                refIsn
+            }
+        }`;
     }
 }
